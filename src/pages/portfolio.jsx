bdf7cac7dfffc935e58eca85cecd97a4b9fba8d8
Jason <private@petstergaming.com>
2022-04-21 23:50:57 -0400

    MERN stack

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
-
+//https://www.mongodb.com/languages/mern-stack-tutorial
 const portfolio = () => {
   return (
     <Layout title="Jason Palmeri | Work & Skills">
